af81e78853f21c15c5368e505244cf9a9dcc9101
Anthony-YV0908 <anthony.yuvega@radenta.com>
2025-08-07 08:16:37 +0800

    initial commit

--- app/Http/Controllers/API/Auth/LoginController.php
+++ app/Http/Controllers/API/Auth/LoginController.php
@@ -59,7 +59,7 @@ class LoginController extends Controller
         $request->user()->currentAccessToken()->delete();
 
         return response()->json([
-            'message' => 'Logged out successfully',
+            'message' => 'Logged out successfull',
         ]);
     }
 }
