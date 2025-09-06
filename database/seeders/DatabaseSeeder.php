e345dbd959be2dafdf320adb54ed73272f5b1432
Anthony-0908 <anthonyyuvega09@gmail.com>
2025-09-06 19:21:10 +0800

    commented the user factory create

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,10 +17,10 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
         $this->call(RoleSeeder::class);
         $this->call(UserRoleSeeder::class);
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
 
          $this->call([
         UserSeeder::class,
