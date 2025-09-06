3e72fc2d90189e01e534a65cb06f3990fa82a039
Anthony-0908 <anthonyyuvega09@gmail.com>
2025-09-06 19:21:25 +0800

    user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,12 +18,13 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-        'photo',
-        'name',
+        // 'photo',
+        'first_name',
+        'last_name',
         'email',
-        'address',
+        // 'address',
         'phone_no',
-        'gender',
+        // 'gender',
         'birthdate',
         'password',
     ];
