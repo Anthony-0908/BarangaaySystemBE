7d44f652fcbaf3fb8cf4c3c3d4de3910c7a6fdaf
Anthony-0908 <anthonyyuvega09@gmail.com>
2025-09-14 00:41:08 +0800

    reset password notification

---
+++ app/Notifications/ResetPasswordNotification.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Auth\Notifications\ResetPassword as ResetPasswordBase;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class ResetPasswordNotification extends ResetPasswordBase
+{
+    protected $url;
+
+    public function __construct($token, $url)
+    {
+        parent::__construct($token);
+        $this->url = $url;
+    }
+
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->subject('Reset Password Notification')
+            ->line('You are receiving this email because we received a password reset request for your account.')
+            ->action('Reset Password', $this->url)
+            ->line('If you did not request a password reset, no further action is required.');
+    }
+}
