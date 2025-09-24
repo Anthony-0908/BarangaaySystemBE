4fbc86fc944396d5dab05119d37f93c2e88b91a9
Anthony-0908 <anthonyyuvega09@gmail.com>
2025-09-24 22:39:01 +0800

    initial commit

---
+++ app/Http/Controllers/API/Admin/AdminController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\API\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Model\User;
+
+class AdminController extends Controller
+{
+    public function index() 
+    {
+
+    }
+
+    public function store()
+    {
+        
+    }
+
+    public function edit()
+    {
+
+    }
+
+    public function destroy() 
+    {
+
+    }
+}
