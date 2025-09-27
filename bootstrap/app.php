8209fb719028c9d5f4d8ce6b1cec71971ba058cc
Anthony-0908 <anthonyyuvega09@gmail.com>
2025-09-27 22:47:51 +0800

    added middleware allias for roles , permission

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,11 +11,12 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware) {[
-        'role' => \App\Http\Middleware\CheckRole::class,
-    ];
-        //
-          
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->alias([
+            'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
+            'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
+            'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
